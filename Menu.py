9f7a27c8c20e94ee5648917ef39ffadb2c28d6ae
JadeDk <office-pierre@dpfl.fr>
2016-06-06 19:59:21 +0200

    New changelist1

--- Menu.py
+++ Menu.py
@@ -1,60 +1,96 @@
+# -*- coding: utf-8 -*-
+
 import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 from PRINCIPAL import *
 from Pluie import *
 
-class Hydraulique_menu(QMainWindow):
-    def __init__(self, parent=None):
-        super(Hydraulique_menu, self).__init__(parent)
-        self.resize(670, 350)
-        self.childWindowHyd = None
-        layout = QHBoxLayout()
 
-        self.bar = self.menuBar()
-        self.file = self.bar.addMenu("Fichier")
-        self.file.addAction("Nouvelle operation")
+class Ui_Menu(object):
+    def setupUi(self, Menu):
+        Menu.setObjectName("Menu")
+        Menu.resize(782, 395)
+        self.centralwidget = QtWidgets.QWidget(Menu)
+        self.centralwidget.setObjectName("centralwidget")
+        Menu.setCentralWidget(self.centralwidget)
 
-        self.save = QAction("Sauvegarde", self)
-        self.save.setShortcut("Ctrl+S")
-        self.file.addAction(self.save)
+        self.menubar = QtWidgets.QMenuBar(Menu)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 782, 21))
+        self.menubar.setObjectName("menubar")
 
-        self.test = self.file.addMenu("Test")
+        self.menuFichier = QtWidgets.QMenu(self.menubar)
+        self.menuFichier.setObjectName("menuFichier")
+        self.menuHydraulique = QtWidgets.QMenu(self.menubar)
+        self.menuHydraulique.setObjectName("menuHydraulique")
+        self.menuBassins = QtWidgets.QMenu(self.menubar)
+        self.menuBassins.setObjectName("menuBassins")
+        Menu.setMenuBar(self.menubar)
 
-        self.ville = QAction("Ville", self)
-        self.ville.setStatusTip('Ville')
-        self.ville.triggered.connect(self.ville_process)
-        self.test.addAction(self.ville)
+        self.statusbar = QtWidgets.QStatusBar(Menu)
+        self.statusbar.setObjectName("statusbar")
+        Menu.setStatusBar(self.statusbar)
 
-        self.pluie = QAction("Pluie", self)
-        self.pluie.setStatusTip('Pluie')
-        self.pluie.triggered.connect(self.pluie_process)
-        self.test.addAction(self.pluie)
+        self.actionNouvelle_affaire = QtWidgets.QAction(Menu)
+        self.actionNouvelle_affaire.setObjectName("actionNouvelle_affaire")
+        self.actionChargement_affaire = QtWidgets.QAction(Menu)
+        self.actionChargement_affaire.setObjectName("actionChargement_affaire")
+        self.actionQuitter = QtWidgets.QAction(Menu)
+        self.actionQuitter.setObjectName("actionQuitter")
+        self.actionVilles = QtWidgets.QAction(Menu)
+        self.actionVilles.setObjectName("actionVilles")
+        self.actionMontana = QtWidgets.QAction(Menu)
+        self.actionMontana.setObjectName("actionMontana")
+        self.actionR_tention = QtWidgets.QAction(Menu)
+        self.actionR_tention.setObjectName("actionR_tention")
+        self.actionInfiltration = QtWidgets.QAction(Menu)
+        self.actionInfiltration.setObjectName("actionInfiltration")
+        self.menuFichier.addAction(self.actionNouvelle_affaire)
+        self.menuFichier.addAction(self.actionChargement_affaire)
+        self.menuFichier.addSeparator()
+        self.menuFichier.addAction(self.actionQuitter)
 
-        self.quit = QAction("Quitter", self)
-        self.quit.triggered.connect(self.processtrigger)
-        self.file.addAction(self.quit)
+        self.menuHydraulique.addAction(self.actionVilles)
+        self.menuHydraulique.addAction(self.actionMontana)
 
-        self.setLayout(layout)
-        self.setWindowTitle("Hydraulique urbaine")
+        self.menuBassins.addAction(self.actionR_tention)
+        self.menuBassins.addAction(self.actionInfiltration)
+        self.menubar.addAction(self.menuFichier.menuAction())
+        self.menubar.addAction(self.menuHydraulique.menuAction())
+        self.menubar.addAction(self.menuBassins.menuAction())
 
-    def processtrigger(self, q):
+        self.retranslateUi(Menu)
+        QtCore.QMetaObject.connectSlotsByName(Menu)
+
+    def retranslateUi(self, Menu):
+        _translate = QtCore.QCoreApplication.translate
+        Menu.setWindowTitle(_translate("Menu", "Hydraulique Urbaine"))
+        self.menuFichier.setTitle(_translate("Menu", "Fichier"))
+        self.menuHydraulique.setTitle(_translate("Menu", "Hydraulique"))
+        self.menuBassins.setTitle(_translate("Menu", "Bassins"))
+        self.actionNouvelle_affaire.setText(_translate("Menu", "Nouvelle affaire"))
+        self.actionChargement_affaire.setText(_translate("Menu", "Chargement affaire"))
+        self.actionQuitter.setText(_translate("Menu", "Quitter"))
+        self.actionVilles.setText(_translate("Menu", "Villes"))
+        self.actionMontana.setText(_translate("Menu", "Montana"))
+        self.actionR_tention.setText(_translate("Menu", "Rétention"))
+        self.actionInfiltration.setText(_translate("Menu", "Infiltration"))
+
+    def actionQuitter(self, q):
         print(q.text() + " ok")
-        self.QApplication.exit()
+        # self.QApplication.exit()
 
-    def ville_process(self):
+    def actionVilles(self):
         self.childWindowHyd = Principal()
         self.childWindowHyd.show()
 
-    def pluie_process(self):
+    def actionMontana(self):
         self.childWindowHyd = pluie()
         self.childWindowHyd.show()
 
-def main():
-    app = QApplication(sys.argv)
-    ex = Hydraulique_menu()
-    ex.show()
-    sys.exit(app.exec_())
-
-
 if __name__ == '__main__':
-    main()
+    app = QtGui.QApplication(sys.argv)
+    Ui_Menu = Ui_Menu()
+    Ui_Menu.main()
+    app.exec_()
